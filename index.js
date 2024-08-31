6bb1d0ceb188c1f34eb92472e0fe19eb6971582c
Lautaro Perrotti <lautaroperrotti@gmail.com>
2024-08-31 00:13:07 -0300

    remove os check within index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 import { generateAllKeys } from "./src/generateAllKeys.js";
-import os from "os";
-
-const platform = os.platform();
-
-if (platform === "win32") {
-  console.log("Running on Windows");
-} else if (platform === "darwin") {
-  console.log("Running on macOS");
-} else if (platform === "linux") {
-  console.log("Running on Linux");
-} else {
-  console.error("Unsupported platform:", platform);
-}
 
 generateAllKeys()
   .then((keys) => console.log(keys))
